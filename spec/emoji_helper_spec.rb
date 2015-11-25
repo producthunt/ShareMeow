e82ec00b44fe914d623099e328422faf878d507a
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-11-25 15:59:28 -0800

    Add test for emoji helper

---
+++ spec/emoji_helper_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+RSpec.describe EmojiHelper do
+  describe '.emojify' do
+    it 'turns unicode into emoji' do
+      content = described_class.emojify('My emoji content 😀')
+
+      expect(content).to eq "My emoji content <img alt=\"grinning\" src=\"#{ShareMeow::App.base_url}/images/emoji/unicode/1f600.png\" " \
+                            "style=\"vertical-align:middle\" width=\"20\" height=\"20\" />"
+    end
+
+    it 'converts GitHub/Slack style emoji' do
+      content = described_class.emojify(':smiley:')
+
+      expect(content).to eq "<img alt=\"smiley\" src=\"#{ShareMeow::App.base_url}/images/emoji/unicode/1f603.png\" " \
+                            "style=\"vertical-align:middle\" width=\"20\" height=\"20\" />"
+    end
+  end
+end
