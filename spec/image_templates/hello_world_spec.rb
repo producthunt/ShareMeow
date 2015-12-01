2c356051d2612354e80097653c7a9b8bb9f28cb6
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-11-30 16:23:14 -0800

    Add Hello World example template

--- spec/image_templates/hello_world_spec.rb
+++ spec/image_templates/hello_world_spec.rb
@@ -2,11 +2,13 @@ require 'spec_helper'
 require 'image_templates/shared_examples'
 
 RSpec.describe ImageTemplates::HelloWorld do
+  default_options = { 'message' => 'Hello, world' }
+
   describe '#to_html' do
-    it 'renders Hello, World' do
-      expect(described_class.new.to_html).to include 'Hello, world'
+    it 'renders the message' do
+      expect(described_class.new(default_options).to_html).to include 'Hello, world'
     end
   end
 
-  it_behaves_like 'an ImageTemplate'
+  it_behaves_like 'an ImageTemplate', default_options
 end
