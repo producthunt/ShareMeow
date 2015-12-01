2d948b3843bb359700f345e8c77bf172bcacf44c
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-11-30 16:30:17 -0800

    1st pass at template for Product Hunt comments

--- app/image_templates/comment.rb
+++ app/image_templates/comment.rb
@@ -1,11 +1,29 @@
+require 'app/emoji_helper'
+
 module ImageTemplates
   class Comment < Base
+    def render_options
+      @options[:name] = EmojiHelper.emojify(@options[:name])
+      @options[:tagline] = EmojiHelper.emojify(@options[:tagline])
+      @options[:content] = EmojiHelper.emojify(@options[:content])
+
+      super
+    end
+
+    def allowed_options
+      %w(content name tagline avatar_url)
+    end
+
     def erb_template
       'app/views/comment.erb'
     end
 
+    def css_stylesheet
+      'app/css/comment.css'
+    end
+
     def image_width
-      300
+      450
     end
   end
 end
--- spec/image_templates/comment_spec.rb
+++ spec/image_templates/comment_spec.rb
@@ -2,21 +2,35 @@ require 'spec_helper'
 require 'image_templates/shared_examples'
 
 RSpec.describe ImageTemplates::Comment do
-  default_options = { name: 'William Shakespurr', tagline: 'Official Spokescat of ProductHunt Books', content: 'Meow' }
+  default_options = { 'name' => 'William Shakespurr',
+                      'tagline' => 'Official Spokescat of ProductHunt Books',
+                      'content' => 'Meow' }
 
   let(:image_template) { described_class.new(default_options) }
 
   describe '#to_html' do
     it 'renders the name' do
-      expect(image_template.to_html).to include default_options[:name]
+      expect(image_template.to_html).to include default_options['name']
     end
 
     it 'renders the tagline' do
-      expect(image_template.to_html).to include default_options[:tagline]
+      expect(image_template.to_html).to include default_options['tagline']
     end
 
     it 'renders the comment contents' do
-      expect(image_template.to_html).to include default_options[:content]
+      expect(image_template.to_html).to include default_options['content']
+    end
+  end
+
+  describe '#render_options' do
+    it 'emojifies the name, tagline & content' do
+      allow(EmojiHelper).to receive(:emojify).and_call_original
+
+      image_template.render_options
+
+      expect(EmojiHelper).to have_received(:emojify).with(default_options['name'])
+      expect(EmojiHelper).to have_received(:emojify).with(default_options['tagline'])
+      expect(EmojiHelper).to have_received(:emojify).with(default_options['content'])
     end
   end
 
