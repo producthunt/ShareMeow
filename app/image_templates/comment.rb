f071b687ecfd469195f04a2f7002f1e7fff2db75
Mike Coutermarsh <coutermarsh.mike@gmail.com>
2015-12-09 14:20:46 -0800

    Set max length for subject_name's
    When subject_name is really long, it runs into the logo. Truncate it.

--- app/image_templates/comment.rb
+++ app/image_templates/comment.rb
@@ -4,6 +4,7 @@ module ImageTemplates
   class Comment < Base
     def render_options
       @options[:name] = EmojiHelper.emojify(@options[:name])
+      @options[:subject_name] = truncate_text(@options[:subject_name], 35)
       @options[:subject_name] = EmojiHelper.emojify(":speech_balloon: on #{@options[:subject_name]}")
       @options[:content] = EmojiHelper.emojify(@options[:content])
 
@@ -24,6 +25,13 @@ module ImageTemplates
 
     def image_width
       450
+    end
+
+    private
+
+    def truncate_text(text, max_length)
+      return text unless text.length > max_length
+      text[0..max_length].gsub(/\s\w+\s*$/,'...')
     end
   end
 end
