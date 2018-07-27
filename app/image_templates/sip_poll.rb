92161a5a1f1fd221abd1a864bfe87bc9a150bcff
Kristian Freeman <kristian@kristianfreeman.com>
2018-07-27 13:36:32 -0500

    Quickfix for sip_poll CSS stuff

--- app/image_templates/sip_poll.rb
+++ app/image_templates/sip_poll.rb
@@ -1,7 +1,12 @@
 module ImageTemplates
   class SipPoll < Base
+    def render_options
+      @options[:poll_options] = percentage_for_css(@options[:poll_options])
+      super
+    end
+
     def allowed_options
-      %w(poll_question poll_options thumbnail_uuid)
+      %w[poll_question poll_options thumbnail_uuid]
     end
 
     def erb_template
@@ -14,6 +19,26 @@ module ImageTemplates
 
     def image_width
       450
+    end
+
+    private
+
+    def percentage_for_css(poll_options)
+      poll_options.map do |poll_option|
+        # NOTE (kristian): Kinda hacky approach to some wkhtmltoimage limitations by
+        # manually defining an offset percentage for this container
+        percentage = if poll_option['percentage'] > 1
+          poll_option['percentage'] - 1
+        else
+          0
+        end
+
+        {
+          option: poll_option['option'],
+          percentage: poll_option['percentage'],
+          css_percentage: "#{percentage}%"
+        }
+      end
     end
   end
 end
